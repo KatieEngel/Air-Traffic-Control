74549cfe0977eb4fedd305e0a56ef6dd490b7bf5
Katie Engel <katie.engel31006@gmail.com>
2026-08-30 13:59:42 -0400

    Updated map to remove watermark

--- atc-frontend/src/App.vue
+++ atc-frontend/src/App.vue
@@ -312,9 +312,9 @@ const initMap = () => {
   setTimeout(() => { map.value.invalidateSize(); }, 100);
 
   // DARK MODE MAP TILES
-  L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
+  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+    maxNativeZoom: 16,
     maxZoom: 20
   }).addTo(map.value);
 
